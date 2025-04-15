59d225b0934240e687d9e8b8a9493fc97d354db3
NifeTeye <151186986+NifeTeye@users.noreply.github.com>
2025-04-14 20:06:09 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-ollama serve &
-sleep 10
-ollama pull starcoder:1b
-tail -f /dev/null
+export OLLAMA_NO_GPU=1
+export OLLAMA_MAX_LOADED_MODELS=1
+curl -fsSL https://ollama.com/install.sh | sh
+ollama pull tinyllama
+ollama serve --max-memory 400M
